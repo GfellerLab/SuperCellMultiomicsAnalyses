5cb4ac621c1142ddb836ad47a80fb97a9a51eb99
aurelieGabriel <gabbrielaureli7@gmail.com>
2025-07-31 13:20:43 +0200

    use correct sif file for bmciteseq

--- snakemakeWorkflows/bmCiteSeq/snakefile.py
+++ snakemakeWorkflows/bmCiteSeq/snakefile.py
@@ -3,7 +3,7 @@
 #################################################################################################
 rule install_seurat_bmcite:
   output: "input/bmCiteSeq/bmcite.rds"
-  singularity: "config/matk_multiomics.sif"
+  singularity: config["sif_file"]
   shell: "Rscript -e 'library(Seurat);.libPaths(c(\"./input/\", .libPaths()));\
                       SeuratData::InstallData(\"bmcite\",lib = \"./input/\");\
                       bm <- SeuratData::LoadData(ds = \"bmcite\");\
@@ -12,15 +12,15 @@ rule install_seurat_bmcite:
 rule singlecells_rna_pca_analysis_bm_cite:
   input: dataset=  "input/bmCiteSeq/bmcite.rds"
   output: "output/bmCiteSeq/singlecells_analysis/seurat.RNA.h5ad",
-  singularity: "config/matk_multiomics.sif"
+  singularity: config["sif_file"]
   benchmark: "benchmark/bmCiteSeq/singlecells_analysis/seurat.RNA.txt"
   shell: "Rscript snakemakeWorkflows/bmCiteSeq/R/RNA_pca_CiteSeqCL.R -i {input.dataset} \
           -o output/bmCiteSeq/singlecells_analysis"
-          
+
 rule singlecells_adt_pca_analysis_bm_cite:
   input: dataset=  "input/bmCiteSeq/bmcite.rds"
   output: "output/bmCiteSeq/singlecells_analysis/seurat.ADT.h5ad",
-  singularity: "config/matk_multiomics.sif"
+  singularity: config["sif_file"]
   benchmark: "benchmark/bmCiteSeq/singlecells_analysis/seurat.ADT.txt"
   shell: "Rscript snakemakeWorkflows/bmCiteSeq/R/ADT_pca_CiteSeqCL.R -i {input.dataset} \
           -o output/bmCiteSeq/singlecells_analysis"
@@ -28,15 +28,15 @@ rule singlecells_adt_pca_analysis_bm_cite:
 rule preprocessing_supercell_bm_cite:
   input: dataset=  "input/bmCiteSeq/bmcite.rds"
   output: "output/bmCiteSeq/singlecells_analysis/seurat_multimodal.rds",
-  singularity: "config/matk_multiomics.sif"
+  singularity: config["sif_file"]
   benchmark: "benchmark/bmCiteSeq/singlecells_analysis/seurat_multimodal.txt"
   shell: "Rscript snakemakeWorkflows/bmCiteSeq/R/preprocessing_seurat_for_SuperCell_CiteSeqCL.R -i {input.dataset} \
           -o output/bmCiteSeq/singlecells_analysis "
-          
+
 rule singlecells_wnn_analysis_bm_cite:
   input: dataset=  "input/bmCiteSeq/bmcite.rds"
   output: "output/bmCiteSeq/singlecells_analysis/seuratWNN.rds"
-  singularity: "config/matk_multiomics.sif"
+  singularity: config["sif_file"]
   params: python = "/opt/conda/envs/MetacellAnalysisToolkit/bin/python3.9"
   shell: "Rscript snakemakeWorkflows/bmCiteSeq/R/wnnAnalysisBM_CiteCL.R -i {input.dataset} \
           -o output/bmCiteSeq/singlecells_analysis \
@@ -45,27 +45,27 @@ rule singlecells_wnn_analysis_bm_cite:
           -q 1:18"
 
 rule metacell_identification_bm_cite:
-  input: 
+  input:
         singlecells = "output/bmCiteSeq/singlecells_analysis/seurat_multimodal.rds"
   output: "output/bmCiteSeq/SuperCellMulti/g{gamma}/SuperCellHierarchy.rds"
-  singularity: "config/matk_multiomics.sif"
+  singularity: config["sif_file"]
   params: workdir = wdir
   benchmark :  "benchmark/bmCiteSeq/SuperCellMulti/g{gamma}/benchIdentification.txt"
   shell: "Rscript R/SCimplifyCiteSeq_v5_CL.R -i {input.singlecells} \
           -o output/bmCiteSeq/SuperCellMulti/g{wildcards.gamma}/ \
           -p 1:30 -q 1:18 -e TRUE -k 30 -g {wildcards.gamma} \
           -s SuperCellHierarchy"
-          
+
 rule data_aggregation_bm_multi:
-  input: 
+  input:
         singlecells = "output/bmCiteSeq/singlecells_analysis/seuratWNN.rds",
         hierarchy = "output/bmCiteSeq/SuperCellMulti/g{gamma}/SuperCellHierarchy.rds",
         gene_protein = "input/bmCiteSeq/gene_protein.csv"
   output: "output/bmCiteSeq/SuperCellMulti/g{gamma}/seurat.cite.mc.rds",
           "output/bmCiteSeq/SuperCellMulti/g{gamma}/metaData.csv",
           "output/bmCiteSeq/SuperCellMulti/g{gamma}/corrTablePearson.csv"
-          
-  singularity: "config/matk_multiomics.sif"
+
+  singularity: config["sif_file"]
   params: workdir = wdir,
           python = "/opt/conda/envs/MetacellAnalysisToolkit/bin/python3.9"
   shell: "Rscript R/SCimplifyCiteSeq_v5_CL.R -i {input.singlecells} -m {input.hierarchy} \
@@ -75,15 +75,15 @@ rule data_aggregation_bm_multi:
           -z {input.gene_protein}\
           -t {params.python}\
           -s seurat"
-          
 
 
-          
+
+
 rule metacell_identification_bm_RNA:
-  input: 
+  input:
         singlecells = "output/bmCiteSeq/singlecells_analysis/seurat.RNA.h5ad"
   output: "output/bmCiteSeq/SuperCellRNA/g{gamma}/SuperCellHierarchy.rds"
-  singularity: "config/matk_multiomics.sif"
+  singularity: config["sif_file"]
   params: workdir = wdir
   benchmark : "benchmark/bmCiteSeq/SuperCellRNA/g{gamma}/benchIdentification.txt"
   shell: "Rscript R/SCimplifyCiteSeq_v5_CL.R -i {input.singlecells} \
@@ -92,14 +92,14 @@ rule metacell_identification_bm_RNA:
           -s SuperCellHierarchy"
 
 rule data_aggregation_bm_RNA:
-  input: 
+  input:
         singlecells = "output/bmCiteSeq/singlecells_analysis/seuratWNN.rds",
         hierarchy = "output/bmCiteSeq/SuperCellRNA/g{gamma}/SuperCellHierarchy.rds",
         gene_protein = "input/bmCiteSeq/gene_protein.csv"
   output: "output/bmCiteSeq/SuperCellRNA/g{gamma}/seurat.cite.mc.rds",
           "output/bmCiteSeq/SuperCellRNA/g{gamma}/metaData.csv",
           "output/bmCiteSeq/SuperCellRNA/g{gamma}/corrTablePearson.csv"
-  singularity: "config/matk_multiomics.sif"
+  singularity: config["sif_file"]
   params: workdir = wdir,
           python = "/opt/conda/envs/MetacellAnalysisToolkit/bin/python3.9"
   shell: "Rscript R/SCimplifyCiteSeq_v5_CL.R -i {input.singlecells} -m {input.hierarchy} \
@@ -109,30 +109,30 @@ rule data_aggregation_bm_RNA:
           -z {input.gene_protein}\
           -t {params.python}\
           -s seurat"
-          
-          
-          
+
+
+
 rule metacell_identification_bm_ADT:
-  input: 
+  input:
         singlecells = "output/bmCiteSeq/singlecells_analysis/seurat.ADT.h5ad"
   output: "output/bmCiteSeq/SuperCellADT/g{gamma}/SuperCellHierarchy.rds"
-  singularity: "config/matk_multiomics.sif"
+  singularity: config["sif_file"]
   params: workdir = wdir
   benchmark : "benchmark/bmCiteSeq/SuperCellADT/g{gamma}/benchIdentification.txt"
   shell: "Rscript R/SCimplifyCiteSeq_v5_CL.R -i {input.singlecells} \
           -o output/bmCiteSeq/SuperCellADT/g{wildcards.gamma}/ \
           -q 1:18 -e TRUE -k 30 -g {wildcards.gamma} \
           -s SuperCellHierarchy"
-          
+
 rule data_aggregation_bm_ADT:
-  input: 
+  input:
         singlecells = "output/bmCiteSeq/singlecells_analysis/seuratWNN.rds",
         hierarchy = "output/bmCiteSeq/SuperCellADT/g{gamma}/SuperCellHierarchy.rds",
         gene_protein = "input/bmCiteSeq/gene_protein.csv"
   output: "output/bmCiteSeq/SuperCellADT/g{gamma}/seurat.cite.mc.rds",
           "output/bmCiteSeq/SuperCellADT/g{gamma}/metaData.csv",
           "output/bmCiteSeq/SuperCellADT/g{gamma}/corrTablePearson.csv"
-  singularity: "config/matk_multiomics.sif"
+  singularity: config["sif_file"]
   params: workdir = wdir,
           python = "/opt/conda/envs/MetacellAnalysisToolkit/bin/python3.9"
   shell: "Rscript R/SCimplifyCiteSeq_v5_CL.R -i {input.singlecells} -m {input.hierarchy} \
@@ -142,28 +142,28 @@ rule data_aggregation_bm_ADT:
           -z {input.gene_protein} \
           -t {params.python}\
           -s seurat"
-          
-          
+
+
 
 rule metacell_identification_bm_seacellsRNA:
-  input: 
+  input:
         singlecells = "output/bmCiteSeq/singlecells_analysis/seurat.RNA.h5ad"
   output: "output/bmCiteSeq/seacellsRNA/g{gamma}/seacellMemberships.csv"
-  singularity: "config/matk_multiomics.sif"
+  singularity: config["sif_file"]
   benchmark: "benchmark/bmCiteSeq/seacellsRNA/g{gamma}/benchIdentification.txt"
   shell: "python3.9 python/SEACellsCL.py -i {input.singlecells} \
           -o output/bmCiteSeq/seacellsRNA/g{wildcards.gamma}/ \
           -d 1:30 -r pca -g {wildcards.gamma}"
-          
+
 rule data_aggregation_bm_seacellsRNA:
-  input: 
+  input:
         singlecells = "output/bmCiteSeq/singlecells_analysis/seuratWNN.rds",
         memberships = "output/bmCiteSeq/seacellsRNA/g{gamma}/seacellMemberships.csv",
         gene_protein = "input/bmCiteSeq/gene_protein.csv"
   output: "output/bmCiteSeq/seacellsRNA/g{gamma}/seurat.cite.mc.rds",
           "output/bmCiteSeq/seacellsRNA/g{gamma}/metaData.csv",
           "output/bmCiteSeq/seacellsRNA/g{gamma}/corrTablePearson.csv"
-  singularity: "config/matk_multiomics.sif"
+  singularity: config["sif_file"]
   params: workdir = wdir,
           python = "/opt/conda/envs/MetacellAnalysisToolkit/bin/python3.9"
   shell: "Rscript R/SCimplifyCiteSeq_v5_CL.R -i {input.singlecells} -c {input.memberships} \
@@ -173,26 +173,26 @@ rule data_aggregation_bm_seacellsRNA:
           -z {input.gene_protein} \
           -t {params.python}\
           -s seurat"
-          
+
 rule metacell_identification_bm_MetaCellRNA:
-  input: 
+  input:
         singlecells = "output/bmCiteSeq/singlecells_analysis/seurat.RNA.h5ad"
   output: "output/bmCiteSeq/MetaCellRNA/g{gamma}/MetaCellMemberships.csv"
   benchmark: "benchmark/bmCiteSeq/MetaCellRNA/g{gamma}/benchIdentification.txt"
-  singularity: "config/matk_multiomics.sif"
+  singularity: config["sif_file"]
   shell: "python3 python/MATK_MetaCell2CL.py -i {input.singlecells} \
           -o output/bmCiteSeq/MetaCellRNA/g{wildcards.gamma}/ \
           -g {wildcards.gamma}"
-          
+
 rule data_aggregation_bm_MetaCellRNA:
-  input: 
+  input:
         singlecells = "output/bmCiteSeq/singlecells_analysis/seuratWNN.rds",
         memberships = "output/bmCiteSeq/MetaCellRNA/g{gamma}/MetaCellMemberships.csv",
         gene_protein = "input/bmCiteSeq/gene_protein.csv"
   output: "output/bmCiteSeq/MetaCellRNA/g{gamma}/seurat.cite.mc.rds",
           "output/bmCiteSeq/MetaCellRNA/g{gamma}/metaData.csv",
           "output/bmCiteSeq/MetaCellRNA/g{gamma}/corrTablePearson.csv"
-  singularity: "config/matk_multiomics.sif"
+  singularity: config["sif_file"]
   params: workdir = wdir,
           python = "/opt/conda/envs/MetacellAnalysisToolkit/bin/python3.9"
   shell: "Rscript R/SCimplifyCiteSeq_v5_CL.R -i {input.singlecells} -c {input.memberships} \
@@ -202,26 +202,26 @@ rule data_aggregation_bm_MetaCellRNA:
           -z {input.gene_protein} \
           -t {params.python}\
           -s seurat"
-          
+
 rule metacell_identification_bm_seacellsADT:
-  input: 
+  input:
         singlecells = "output/bmCiteSeq/singlecells_analysis/seurat.ADT.h5ad",
   output: "output/bmCiteSeq/seacellsADT/g{gamma}/seacellMemberships.csv"
-  singularity: "config/matk_multiomics.sif"
+  singularity: config["sif_file"]
   benchmark:"benchmark/bmCiteSeq/seacellsADT/g{gamma}/benchIdentification.txt"
   shell: "python3.9 python/SEACellsCL.py -i {input.singlecells} \
           -o output/bmCiteSeq/seacellsADT/g{wildcards.gamma}/ \
           -d 1:18 -r apca -g {wildcards.gamma}"
-          
+
 rule data_aggregation_bm_seacellsADT:
-  input: 
+  input:
         singlecells = "output/bmCiteSeq/singlecells_analysis/seuratWNN.rds",
         memberships = "output/bmCiteSeq/seacellsADT/g{gamma}/seacellMemberships.csv",
         gene_protein = "input/bmCiteSeq/gene_protein.csv"
   output: "output/bmCiteSeq/seacellsADT/g{gamma}/seurat.cite.mc.rds",
           "output/bmCiteSeq/seacellsADT/g{gamma}/metaData.csv",
           "output/bmCiteSeq/seacellsADT/g{gamma}/corrTablePearson.csv"
-  singularity: "config/matk_multiomics.sif"
+  singularity: config["sif_file"]
   params: workdir = wdir,
           python = "/opt/conda/envs/MetacellAnalysisToolkit/bin/python3.9"
   shell: "Rscript R/SCimplifyCiteSeq_v5_CL.R -i {input.singlecells} -c {input.memberships} \
@@ -231,15 +231,15 @@ rule data_aggregation_bm_seacellsADT:
           -z {input.gene_protein} \
           -t {params.python} \
           -s seurat"
-          
+
 rule random_metacell_bm_multiome:
-  input: 
+  input:
         singlecells = "output/bmCiteSeq/singlecells_analysis/seuratWNN.rds",
         gene_protein = "input/bmCiteSeq/gene_protein.csv"
   output: "output/bmCiteSeq/randomMetacells/g{gamma}/seurat.cite.mc.rds",
           "output/bmCiteSeq/randomMetacells/g{gamma}/metaData.csv",
            "output/bmCiteSeq/randomMetacells/g{gamma}/corrTablePearson.csv"
-  singularity: "config/matk_multiomics.sif"
+  singularity: config["sif_file"]
   params: workdir = wdir,
           python = "/opt/conda/envs/MetacellAnalysisToolkit/bin/python3.9"
   shell: "Rscript R/SCimplifyCiteSeq_v5_CL.R -i {input.singlecells} -d TRUE \
@@ -258,10 +258,10 @@ rule random_metacell_bm_multiome:
 
 
 rule Semi_sup_test_bm_cite:
-  input: 
+  input:
         singlecells = "output/bmCiteSeq/singlecells_analysis/seuratWNN.rds"
   output: "output/bmCiteSeq/SuperCellMulti/testSemiSup/g{graining}/results_test_semisup.csv"
-  singularity: "config/matk_multiomics.sif"
+  singularity: config["sif_file"]
   params: workdir = wdir
   # benchmark :  "benchmark/pbmcMultiome/SuperCellMulti/g{graining}/seurat.multiome.mc.txt"
   shell: "Rscript R/SCimplify_test_semi_sup_CL.R -i {input.singlecells} \
@@ -269,16 +269,16 @@ rule Semi_sup_test_bm_cite:
           -p 1:30 -q 1:18 -r RNA -a ADT -v pca -w apca -g {wildcards.graining} -s 2025 -l celltype.l2"
 
 rule Semi_sup_test_bm_cite_rna:
-  input: 
+  input:
         singlecells = "output/bmCiteSeq/singlecells_analysis/seuratWNN.rds"
   output: "output/bmCiteSeq/SuperCellRNA/testSemiSup/g{graining}/results_test_semisup.csv"
-  singularity: "config/matk_multiomics.sif"
+  singularity: config["sif_file"]
   params: workdir = wdir
   # benchmark :  "benchmark/pbmcMultiome/SuperCellMulti/g{graining}/seurat.multiome.mc.txt"
   shell: "Rscript R/SCimplify_test_semi_sup_CL.R -i {input.singlecells} \
           -o output/bmCiteSeq/SuperCellRNA/testSemiSup/g{wildcards.graining}/ \
           -p 1:30 -r RNA -v pca -g {wildcards.graining} -s 2025 -l celltype.l2"
-          
+
 ####################################################################################################################################
 ####################################################################################################################################
 ####################################################################################################################################
